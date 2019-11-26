9b009ce9af27079e9cc7df10c6c28d90f716540f
kingofclubstroy <jordanchew4@hotmail.com>
2019-11-26 12:00:32 -0500

    Initial commit to the github repo
    setting up this project with github

---
+++ Assets/Scripts/Controllers/WorldController.cs
@@ -0,0 +1,238 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour
+{
+    public GameObject tilePrefab;
+    List<List<TileScript>> tileMap;
+    float size;
+
+    public static WorldController instance;
+
+    static List<TileScript> activeTiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        size = tilePrefab.GetComponent<Renderer>().bounds.size.x;
+        activeTiles = new List<TileScript>();
+        instance = this;
+        populateWorld(40, 40);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spreadHeat();
+    }
+
+
+    void populateWorld(int x, int y)
+    {
+
+        tileMap = new List<List<TileScript>>();
+
+        for (int i = 0; i < x; i++)
+        {
+
+            for (int j = 0; j < y; j++)
+            {
+
+                if (j == 0)
+                {
+                    tileMap.Add(new List<TileScript>());
+                }
+
+                GameObject tile = Instantiate(tilePrefab, new Vector2(transform.position.x + i * size, transform.position.y - j * size), Quaternion.identity);
+
+                TileScript tileScript = tile.GetComponent<TileScript>();
+
+                tileScript.position = new Vector2(i, j);
+
+                float random = Random.Range(0f, 1f);
+
+                tileScript.flamability = Random.Range(0.1f, 10f);
+
+                if (i < 5 && j < 5)
+                {
+                    //the top left corner is on fire
+                    tileScript.onFire = true;
+                    tileScript.fire = Random.Range(0f, 20f);
+                    tileScript.fuel = Random.Range(0f, 20f);
+                }
+
+                //if (random <= 1f)
+                if(i >= x - 5 && j >= y - 5)  
+                {
+                    //80% chance the tile has fuel to burn but isnt on fire
+                    tileScript.fuel = Random.Range(10f, 30f);
+                    tileScript.growing = true;
+                    
+                } 
+              
+                //There is a 20% chance there is no fuel or fire to simulate bare ground
+
+
+                tileMap[i].Add(tileScript);
+
+            }
+
+        }
+
+    }
+
+
+
+
+    List<TileScript> findNeighbours(Vector2 position)
+    {
+
+        //need to find the adjacent neighbours ie: north, east, south, west, northWest, northEast, southEast, southWest 
+        List<TileScript> adjacentList = new List<TileScript>();
+
+        int x = (int) position.x;
+        int y = (int)position.y;
+
+        //North tile
+        adjacentList.Add(getTile(x, y - 1));
+
+        //East
+        adjacentList.Add(getTile(x + 1, y));
+
+        //South
+        adjacentList.Add(getTile(x, y + 1));
+
+        //West
+        adjacentList.Add(getTile(x - 1, y));
+
+        //NorthWest
+        //adjacentList.Add(getTile(x - 1, y - 1));
+
+        //NorthEast
+        //adjacentList.Add(getTile(x + 1, y - 1));
+
+        //SouthEast
+        //adjacentList.Add(getTile(x + 1, y + 1));
+
+        //SouthWest
+        //adjacentList.Add(getTile(x - 1, y + 1));
+
+        return adjacentList;
+
+    }
+
+    TileScript getTile(int x, int y)
+    {
+
+        if(x < 0 || x >= tileMap.Count || y < 0 || y >= tileMap[x].Count)
+        {
+            //Tile is out of range, so doesnt exist.
+            return null;
+        } else
+        {
+            return tileMap[x][y];
+        }
+
+    }
+
+
+    void spreadHeat()
+    {
+
+        //this list is added as a workaround to errors generated when adding to a list that is currently being looped though, need to replace
+        List<TileScript> growthToSet = new List<TileScript>();
+
+        Debug.Log(activeTiles.Count);
+
+        foreach(TileScript mainTile in activeTiles)
+        //for (int i = 0; i < activeTiles.Count; i++)
+        {
+            //TileScript mainTile = activeTiles[i];
+            if (mainTile.onFire)
+            //if (true) 
+            {
+
+                float heat = mainTile.heat;
+
+                List<TileScript> adjacentTiles = findNeighbours(mainTile.position);
+
+                float totalHeatExchanged = 0;
+
+                foreach (TileScript neighbour in adjacentTiles)
+                {
+
+                    if (neighbour != null && !neighbour.onFire && neighbour.fuel > 0)
+                    {
+                           
+                        float difference = (heat - neighbour.heat) / 8;
+
+                        if (difference > 0)
+                        {
+
+                            neighbour.heat += difference;
+                            totalHeatExchanged += difference;
+
+                        }
+                            
+
+
+                    }
+
+                }
+
+                mainTile.heat -= totalHeatExchanged;
+            } else if (mainTile.growing)
+            {
+               
+                List<TileScript> adjacentTiles = findNeighbours(mainTile.position);
+                foreach (TileScript neighbour in adjacentTiles)
+                {
+
+                    if (neighbour != null && !neighbour.onFire && !neighbour.growing)
+                    {
+
+                        float random = Random.Range(0f, 100f);
+
+                        if(random <= (neighbour.fuelGrowth * mainTile.fuel/100 * Time.deltaTime))
+                        {
+                            growthToSet.Add(neighbour);
+                        }
+                        
+
+                    }
+
+                }
+                
+            }/* else
+            {
+                Debug.Log("tile set inactive due to not being on fire or growing, this shouldnt happen");
+                setInactive(mainTile);
+            }*/
+
+            
+        }
+
+        foreach (TileScript tile in growthToSet)
+        {
+            tile.growing = true;
+        }
+
+        growthToSet = null;
+
+    }
+
+    public void setActive(TileScript tile)
+    {
+        activeTiles.Add(tile);
+    }
+
+    public void setInactive(TileScript tile)
+    {
+        activeTiles.Remove(tile);
+    }
+
+}
+
+
---
+++ Assets/Scripts/Models/TileScript.cs
@@ -0,0 +1,239 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+
+    //Is fire the same as heat? like the same value, but only different visually and when spells require it?
+
+    public float fire;
+
+    private float _fuel;
+    public float fuel
+    {
+        get
+        {
+            return _fuel;
+        } set
+        {
+            if (value >= 100)
+            {
+                _fuel = 100;
+            } else
+            {
+                _fuel = value;
+            }
+        }
+    }
+
+    private bool _growing;
+    public bool growing
+    {
+        get
+        {
+            return _growing;
+        } set
+        {
+            _growing = value;
+
+            if(_growing)
+            {
+                WorldController.instance.setActive(this);
+            } else
+            {
+                WorldController.instance.setInactive(this);
+            }
+        }
+    }
+    
+    public float heat;
+    public float growthThreshold;
+
+    public float fuelGrowth;
+
+    public float flamability = 100f;
+    public float ignition = 100f;
+
+    public float fireHeatTransfer = 0f;
+    public float heatSpread = 0.25f;
+    public float fuelToFireConversion = 0f;
+    public float heatLoss = 0f;
+
+    public float neutrients = 0f;
+
+    public float fuelDensity = 3f;
+
+    private SpriteRenderer sprite;
+
+    public Vector2 position;
+
+
+    private bool _onFire = false;
+    public bool onFire
+    {
+        get
+        {
+            return _onFire;
+        }
+        set
+        {
+            _onFire = value;
+
+            if(value)
+            {
+                WorldController.instance.setActive(this);
+            } else
+            {
+                WorldController.instance.setInactive(this);
+            } 
+
+        }
+    }
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        heat = 0;
+
+
+        sprite = GetComponent<SpriteRenderer>();
+
+        flamability = 6f;
+        fuelDensity = 1.5f;
+        fuelGrowth = 20f;
+        growthThreshold = 40f;
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fireUpdate();
+        growthUpdate();
+        spriteUpdate();
+        
+    }
+
+
+    void fireUpdate()
+    {
+        if (onFire)
+        {
+
+            if (fuel <= 0)
+            {
+              
+                // this is an arbituary number, going to change in the future
+                fire -= 20 * Time.deltaTime;
+
+                if (fire <= 0)
+                {
+                    fire = 0;
+                    onFire = false;
+                }
+                
+            }
+            else
+            {
+
+                // is heat the same as fire when an object is on fire?
+                heat = fire;
+
+                fire += 1 * Time.deltaTime * fuelDensity;
+                fuel -= 1 * Time.deltaTime;
+                neutrients += 1 * Time.deltaTime;
+
+                if (fire >= 100)
+                {
+                    fire = 100;
+                }
+                
+
+            }
+
+            
+
+
+        }
+        else 
+        {
+            if (fuel > 0)
+            {
+                if (heat > 0)
+                {
+
+                    //onFire = true;
+                    heat -= Time.deltaTime;
+                    fuel += fuelGrowth * Time.deltaTime;
+
+                    if (heat >= ignition)
+                    {
+                        onFire = true;
+                    }
+                    else
+                    {
+
+                        float random = Random.Range(0f, 100f);
+                        if (random <= (heat * flamability * Time.deltaTime))
+                        {
+                            onFire = true;
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    void growthUpdate()
+    {
+        if(growing && !onFire)
+        {
+
+            fuel += fuelGrowth + (neutrients / 100f) * Time.deltaTime;
+
+        }
+    }
+
+
+    void spriteUpdate()
+    {
+
+        Color mColor;
+
+        if (onFire)
+        {
+
+            mColor = new Color(Mathf.Lerp(0.4f, 1, fire/70), 0, 0);
+
+        } else if(fuel <= 0)
+        {
+
+            if (neutrients > 0)
+            {
+                mColor = new Color(0.5f / neutrients, 0.5f / neutrients, 0.5f / neutrients);
+            }
+            else
+            {
+
+                mColor = new Color(0.4f, 0.37058823529f, 0.37058823529f);
+
+            }
+            
+
+        } else
+        {
+            mColor = new Color(0, Mathf.Lerp(0.1f, 1f, fuel/100), 0); 
+        } 
+
+        sprite.color = mColor;
+
+    }
+
+}
