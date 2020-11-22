14c3aa00ad3650652fc98db5ef31530e0c808227
kingofclubstroy <jordanchew4@hotmail.com>
2020-11-21 17:12:10 -0800

    Improved Animations
    Incorporated changing animations as an ai moves towards target, and cleaned up the base animator controller

--- Assets/Scripts/AI Scripts/Movement/AIMovementHandler.cs
+++ Assets/Scripts/AI Scripts/Movement/AIMovementHandler.cs
@@ -8,6 +8,12 @@ public class AIMovementHandler : MonoBehaviour
     Vector2 targetPosition;
     bool hasTarget = false;
 
+    [SerializeField]
+    int leftAngle = 135;
+
+    [SerializeField]
+    int rightAngle = 45;
+
     public int speed;
 
     [SerializeField]
@@ -36,82 +42,23 @@ public class AIMovementHandler : MonoBehaviour
         if(Input.GetMouseButtonDown(0))
         {
             targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            
             hasTarget = true;
-            float angle = Vector2.SignedAngle(transform.position, targetPosition.normalized);
-            Debug.Log("angle = " + angle);
-            Debug.Log("targetPosition = " + targetPosition);
+            Vector2 testPos = Camera.main.ScreenToWorldPoint(transform.position);
+            float angle = Vector2.SignedAngle(testPos, (Vector2) transform.position - targetPosition);
+           
 
-            // the vector that we want to measure an angle from
-            Vector3 referenceForward = transform.position;/* some vector that is not Vector3.up */
-                                       // the vector perpendicular to referenceForward (90 degrees clockwise)
-                                       // (used to determine if angle is positive or negative)
-            Vector3 referenceRight = Vector3.Cross(Vector3.up, referenceForward);
-            // the vector of interest
-            Vector3 newDirection = targetPosition; /* some vector that we're interested in */
-                                   // Get the angle in degrees between 0 and 180
-            float angle2 = Vector3.Angle(newDirection, referenceForward);
-            // Determine if the degree value should be negative.  Here, a positive value
-            // from the dot product means that our vector is on the right of the reference vector   
-            // whereas a negative value means we're on the left.
-            float sign = Mathf.Sign(Vector3.Dot(newDirection, referenceRight));
-            float finalAngle = sign * angle2;
-
-            Debug.Log("new signed angle = " + finalAngle);
+           
             
         }
 
         if(hasTarget)
         {
 
-            Vector2 dir = (Vector2)(targetPosition - (Vector2)transform.position).normalized;
+            Vector2 dir = (targetPosition - (Vector2)transform.position).normalized;
 
-            float angle = Vector2.SignedAngle(transform.position, targetPosition);
-
-            
-
-            if(dir.x != 0)
-            {
-                if(dir.x > 0)
-                {
-                    if(currentDirection != Direction.RIGHT)
-                    {
-                        currentDirection = Direction.RIGHT;
-                        animator.SetBool("Walking_Up", false);
-                        animator.SetBool("Walking_Down", false);
-                        animator.SetBool("Walking_Left", false);
-                        animator.SetBool("Walking_Right", true);
-
-                    }
-                } else if (currentDirection != Direction.LEFT)
-                {
-                    currentDirection = Direction.LEFT;
-                    animator.SetBool("Walking_Up", false);
-                    animator.SetBool("Walking_Down", false);
-                    animator.SetBool("Walking_Left", true);
-                    animator.SetBool("Walking_Right", false);
-                }
-            } else
-            {
-                if(dir.y > 0)
-                {
-                    if(currentDirection != Direction.UP)
-                    {
-                        currentDirection = Direction.UP;
-                        animator.SetBool("Walking_Up", true);
-                        animator.SetBool("Walking_Down", false);
-                        animator.SetBool("Walking_Left", false);
-                        animator.SetBool("Walking_Right", false);
-                    } 
-                } else if(currentDirection != Direction.Down)
-                {
-                    currentDirection = Direction.Down;
-                    animator.SetBool("Walking_Up", false);
-                    animator.SetBool("Walking_Down", true);
-                    animator.SetBool("Walking_Left", false);
-                    animator.SetBool("Walking_Right", false);
-                }
-            }
-
+            SetAnimationDirection(dir);
 
             float frameSpeed = speed * Time.deltaTime;
 
@@ -130,5 +77,56 @@ public class AIMovementHandler : MonoBehaviour
         }
 
        
+    }
+
+    void SetAnimationDirection(Vector2 dir)
+    {
+        float angle = Vector2.SignedAngle(dir, new Vector2(1, 0));
+
+        if (Mathf.Abs(angle) <= rightAngle)
+        {
+            if (currentDirection != Direction.RIGHT)
+            {
+                currentDirection = Direction.RIGHT;
+                animator.SetBool("Walking_Up", false);
+                animator.SetBool("Walking_Down", false);
+                animator.SetBool("Walking_Left", false);
+                animator.SetBool("Walking_Right", true);
+
+            }
+        }
+        else if (Mathf.Abs(angle) >= leftAngle)
+        {
+            if (currentDirection != Direction.LEFT)
+            {
+                currentDirection = Direction.LEFT;
+                animator.SetBool("Walking_Up", false);
+                animator.SetBool("Walking_Down", false);
+                animator.SetBool("Walking_Left", true);
+                animator.SetBool("Walking_Right", false);
+            }
+        }
+        else if (angle > 0)
+        {
+            if (currentDirection != Direction.Down)
+            {
+                currentDirection = Direction.Down;
+                animator.SetBool("Walking_Up", false);
+                animator.SetBool("Walking_Down", true);
+                animator.SetBool("Walking_Left", false);
+                animator.SetBool("Walking_Right", false);
+            }
+        }
+        else
+        {
+            if (currentDirection != Direction.UP)
+            {
+                currentDirection = Direction.UP;
+                animator.SetBool("Walking_Up", true);
+                animator.SetBool("Walking_Down", false);
+                animator.SetBool("Walking_Left", false);
+                animator.SetBool("Walking_Right", false);
+            }
+        }
     }
 }
