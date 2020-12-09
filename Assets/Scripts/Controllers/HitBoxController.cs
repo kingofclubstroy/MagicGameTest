2354da6717b587444509c65fba31150353806bbe
kingofclubstroy <jordanchew4@hotmail.com>
2020-12-09 12:51:23 -0800

    Added attack up animation

--- Assets/Scripts/Controllers/HitBoxController.cs
+++ Assets/Scripts/Controllers/HitBoxController.cs
@@ -11,17 +11,23 @@ public class HitBoxController : MonoBehaviour
     // Collider on this game object
     private PolygonCollider2D localCollider;
 
+    /// <summary>
+    /// Set the hitboxes for each animation
+    /// </summary>
     [SerializeField]
     AnimationHitboxes[] animationHitboxes;
 
+    //Quality of life dictionary for grabbing appropriate colliders
     Dictionary<string, PolygonCollider2D[]> AnimationDictionary;
 
-    int index = 0;
+    //Indicates index for the next hitbox
+    int hitboxIndex = 0;
 
 
     void Start()
     {
 
+        //Convert serialized array of animation hitbox objects into a more easily accessable dictionary, indexed by name 
         AnimationDictionary = new Dictionary<string, PolygonCollider2D[]>();
 
         foreach(AnimationHitboxes boxes in animationHitboxes)
@@ -41,25 +47,42 @@ public class HitBoxController : MonoBehaviour
     {
         Debug.Log("HitBoxController hit something!");
         Debug.Log(col.gameObject.name);
+        
     }
 
+    /// <summary>
+    /// Tell the controller which animation is running so we can prepare appropriate hitboxes
+    /// </summary>
+    /// <param name="animation"> Name of Animation that is about to exaqute</param>
     public void SetNewAnimation(string animation)
     {
-        colliders = AnimationDictionary[animation];
-        index = 0;
+        if (AnimationDictionary.ContainsKey(animation))
+        {
+            //Grab the appropriate list of coliders and resets the index of current hitbox
+            colliders = AnimationDictionary[animation];
+            hitboxIndex = 0;
+        } else
+        {
+            Debug.LogError("No hitboxes set for this attack");
+        }
     }
 
+    /// <summary>
+    /// Called by animation triggers to set the next appropriate hitbox or clear the last one
+    /// </summary>
     public void setHitBox()
     {
-        if (index >= colliders.Length)
+        if (hitboxIndex >= colliders.Length)
         {
+            //index if over hitbox count for this animation, so we will clear the hitbox
             localCollider.pathCount = 0;
             
             return;
         }
 
-        localCollider.SetPath(0, colliders[index].GetPath(0));
-        index++;
+        //Sets the new points for the hitbox, and increment the index for when we set the next hitbox
+        localCollider.SetPath(0, colliders[hitboxIndex].GetPath(0));
+        hitboxIndex++;
        
         
     }
